5d103a9b72f20bb4f85188672009a6d69bc9bdba
Zach Giffen <zach.giffen@yahoo.com>
2023-10-06 21:31:06 +0000

    completed prep-javascript-functions

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,53 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+var convertHoursToMinutesResults = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes exercise:', convertHoursToMinutesResults);
+
+function getGreeting(name) {
+  return 'Hello ' + name;
+}
+var getGreetingResults = getGreeting('World');
+console.log('getGreeting exercise:', getGreetingResults);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+var addAndMultiplyBy5Results = addAndMultiplyBy5(10, 5);
+console.log('addAndMultiplyBy5 exercise:', addAndMultiplyBy5Results);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+var multiplyAndDivideBy5Resultss = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 exercise:', multiplyAndDivideBy5Resultss);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+var subtractTwoNumbersResults = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers exercise:', subtractTwoNumbersResults);
+
+function getCircleCircumfrence(radius) {
+  return 2 * 3.14159 * radius;
+}
+var getCircleCircumfrenceResults = getCircleCircumfrence(5);
+console.log('getCircleCircumfrence exercise:', getCircleCircumfrenceResults);
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+var getFullNameResults = getFullName('Zach', 'Giffen');
+console.log('getFullName exercise:', getFullNameResults);
+
+function cube(number) {
+  return number * number * number;
+}
+var cubeResults = cube(5);
+console.log('cube exercise:', cubeResults);
---
+++ prep-javascript-functions/prep-javascript-functions-quiz-notes.md
@@ -0,0 +1,40 @@
+# prep-javascript-functions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What are the five parts of a function definition?
+  the function keyword, a name, a list in parathenses, a opening and closing curly bracket, a return statement in the brackets
+- How do you call a function?
+  by just calling it like an object, as long as its in the codeblock
+- What is the difference between a parameter and an argument?
+  a paraamter is a placeholder for the argument
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
