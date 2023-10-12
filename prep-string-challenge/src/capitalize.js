ebc20d4a1c97dd7c76029da2c46743f59181d471
Zach Giffen <zach.giffen@yahoo.com>
2023-10-12 21:45:35 +0000

    completed prep-string-challenge

---
+++ prep-string-challenge/src/capitalize.js
@@ -0,0 +1,6 @@
+/* exported capitalize */
+function capitalize(word) {
+  const firstLetter = word[0].toUpperCase();
+  const restOfWord = word.slice(1).toLowerCase();
+  return firstLetter + restOfWord;
+}
